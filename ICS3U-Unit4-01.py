e5ef685e3bb48e435f1ce54bc533f7e8138aaeac
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-07 01:16:46 +0000

    done done doneeeee!!!!!!

--- ICS3U-Unit4-01.py
+++ ICS3U-Unit4-01.py
@@ -5,33 +5,34 @@
 # This program uses a while loop
 
 
-def main():
-    # this function uses a while loop
 
-    # this is to keep track of hw many times you go through the loop
+def main():
+
     loop_counter = 0
     add_int = 0
 
-    # input
-    integer = input("Please enter a positive number: ")
+    # This function adds all the whole numbers up to the inputted number
+
+    # Input
+    positive_string = input("Enter a positive number: ")
     print("")
 
-    # process & output
+    # Process and output
     try:
-        integer = int(integer)
-        if integer > 0:
-            while loop_counter < integer:
+        positive_integer = int(positive_string)
+        if positive_integer > 0:
+            while loop_counter < positive_integer:
                 loop_counter = loop_counter + 1
                 add_int = add_int + loop_counter
-                print(
-                    "The sum of all positive numbers from 1 to {0} is {1}.".format(
-                        integer, add_int
-                    )
+            print(
+                "The sum of all positive numbers from 1 to {0} is {1}.".format(
+                    positive_integer, add_int
                 )
+            )
         else:
-            print("{0} is not a positive integer".format(integer))
+            print("{0} is not a positive integer".format(positive_string))
     except ValueError:
-        print("{0} is not a valid input".format(integer))
+        print("{0} is not a valid input".format(positive_string))
     finally:
         print("\nDone.")
 
